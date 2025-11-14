d938a4ae7086ced932bfdabeb096398cb35a4e1c
Timo Haapala <timo.haapala@geniem.com>
2025-11-14 16:46:11 +0200

    Routing stuff

---
+++ components/hello.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+const Hello = () => {
+    console.log("Hello from the client component!");
+    return <div>Hello</div>;
+}
+
+export default Hello
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
   experimental: {
     turbopackFileSystemCacheForDev: true,
+
   },
 };
 
